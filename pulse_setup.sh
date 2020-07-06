9b930564fed7b57a092102d26db67a3713961685
Abhishek <abhishek.bhakat@hotmail.com>
2020-07-07 00:18:10 +0530

    Update pulse_setup.sh

--- pulse_setup.sh
+++ pulse_setup.sh
@@ -6,26 +6,20 @@
 # pacmd list-sources
 # pacmd list-source-outputs
 # Use pavucontrol to make tests for your setup and to make the runtime configuration
-# Route your audio source to virtual1
-# Record your sound (videoconference) from virtual2.monitor
-
-MICROPHONE="alsa_input.pci-0000_00_1b.0.analog-stereo"
-SPEAKERS="alsa_output.pci-0000_00_1b.0.analog-stereo"
+# Route your audio source to virtualspeaker
+# Record your sound (videoconference) from virtualmic.monitor
 
 
 # Create the null sinks
-# virtual1 gets your audio sources (mplayer ...) that you want to hear and share
-# virtual2 gets all the audio you want to share (virtual1 + micro)
-pactl load-module module-null-sink sink_name=virtual1 sink_properties=device.description="virtual1"
-pactl load-module module-null-sink sink_name=virtual2 sink_properties=device.description="virtual2"
+# virtualspeaker gets your audio sources (mplayer ...) that you want to hear and share
+# virtualmic gets all the audio you want to share (virtualspeaker + micro)
+pactl load-module module-null-sink sink_name=virtualspeaker sink_properties=device.description="virtualspeaker"
+pactl load-module module-null-sink sink_name=virtualmic sink_properties=device.description="virtualmic"
 
 # Now create the loopback devices, all arguments are optional and can be configured with pavucontrol
-pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS latency_msec=1
-pactl load-module module-loopback source=virtual1.monitor sink=virtual2 latency_msec=1
-pactl load-module module-loopback source=$MICROPHONE sink=virtual2 latency_msec=1
-
-# If you struggle to find the correct values of your physical devices, you can
-# also simply leave these undefined, and configure everything manually via pavucontrol
-# pactl load-module module-loopback source=virtual1.monitor
-# pactl load-module module-loopback source=virtual1.monitor sink=virtual2
-# pactl load-module module-loopback sink=virtual2
+# Here sink 0 is source speaker sink (confirm using pacmd list-sinks)
+pactl load-module module-loopback source=virtualspeaker.monitor sink=0 latency_msec=1
+# Here sink 2 is virtualmic (confirm using pacmd list-sinks)
+pactl load-module module-loopback source=virtualspeaker.monitor sink=2 latency_msec=1
+# Here source 1 is source mic (confirm using pacmd list-sources)
+pactl load-module module-loopback source=1 sink=2 latency_msec=1
